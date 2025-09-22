27b7ec0577ee00e3663528e9945d15374dd973d9
Qumu <3552938843@qq.com>
2025-09-22 20:52:12 +0800

    Create example.lua

---
+++ example.lua
@@ -0,0 +1,7 @@
+local key = string.rep("O",256)
+local nonce = string.rep("O",12)
+local msg = "O.O"
+local e = chacha20(key,nonce,0,msg)--encrypt the msg
+print(e)
+local d = chacha20(key,nonce,0,e)--decrypt the ciphertext
+print(d)--should be the same
